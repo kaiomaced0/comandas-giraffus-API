6184f2a8dd909c1f322b9052c17264c5e97e1fcb
Kaio Macedo <kaiomm2000@gmail.com>
2023-08-09 03:38:45 -0300

    criando historico de pagamentos apagados

---
+++ src/main/java/k/model/PagamentoRemovidoHistorico.java
@@ -0,0 +1,37 @@
+package k.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class PagamentoRemovidoHistorico {
+
+    @OneToOne
+    @JoinColumn(name = "pagamento_pagamentoRemovidoHistorico")
+    private Pagamento pagamento;
+
+    @Column(name = "comentario_pagamentoRemovido")
+    private String comentario;
+
+    public Pagamento getPagamento() {
+        return pagamento;
+    }
+
+    public void setPagamento(Pagamento pagamento) {
+        this.pagamento = pagamento;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+}
---
+++ src/main/java/k/repository/PagamentoRemovidoHistoricoRepository.java
@@ -0,0 +1,8 @@
+package k.repository;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import k.model.PagamentoRemovidoHistorico;
+
+public class PagamentoRemovidoHistoricoRepository implements PanacheRepository<PagamentoRemovidoHistorico> {
+
+}
