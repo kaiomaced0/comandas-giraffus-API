022282ae4607b1b54a99ffc932df643fe44f3140
05562849259 <kaiomm2000@gmail.com>
2024-01-17 20:39:33 -0300

    login add UsuariOResponseDTO

--- src/main/java/k/dto/UsuarioResponseDTO.java
+++ src/main/java/k/dto/UsuarioResponseDTO.java
@@ -8,13 +8,14 @@ import k.model.Usuario;
 public record UsuarioResponseDTO(
         Long id,
         String nome,
+        String login,
         String email,
         String cpf,
         List<PerfilResponseDTO> listaPerfil,
         EmpresaResponseDTO empresaResponseDTO) {
 
     public UsuarioResponseDTO(Usuario usuario) {
-        this(usuario.getId(), usuario.getNome(), usuario.getEmail(), usuario.getCpf(),
+        this(usuario.getId(), usuario.getNome(), usuario.getLogin(), usuario.getEmail(), usuario.getCpf(),
                 usuario.getPerfis().stream().map(perfil -> new PerfilResponseDTO(perfil.getId(), perfil.getLabel()))
                         .collect(Collectors.toList()),
                 new EmpresaResponseDTO(usuario.getEmpresa()));
